4331637f431937e040c3497e909a94effef251ee
2017000297-FabianLopez <f.lopez.5cdb@gmail.com>
2026-06-16 18:16:32 -0600

    .

--- Guia 1/C#/Lecc_30_Act1_Inc2_Arreglo1.cs
+++ Guia 1/C#/Lecc_30_Act1_Inc2_Arreglo1.cs
@@ -2,6 +2,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        
+        Console.WriteLine("Nombre: Fabián André Lopez Higueros");
+        Console.WriteLine("Grado y Seccion: IV C");
+        Console.WriteLine("Clave: 19");
+        
         int num = 0;
         String[] color = new string[6];
         color[0]= "";
